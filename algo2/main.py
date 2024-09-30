83b04477cf044f6a4a260ad207fba054ef3d568f
Richard Hong <81711255+Richardhhhg@users.noreply.github.com>
2024-09-30 10:14:30 -0400

    Update main.py

--- algo2/main.py
+++ algo2/main.py
@@ -6,54 +6,54 @@ s = requests.Session()
 s.headers.update({'X-API-key': 'GW4GJ6BI'}) # API Key from YOUR RIT Client
 
 def main():
-    tick, status = get_tick()
     
-    while status == 'ACTIVE':
-        best_bid_price, best_ask_price = get_bid_ask('AC')
-        
-        # position = get_position()
+    MAX_EXPOSURE = 25000
+
+    order_type = 'LIMIT'
+    
+    PREMIUM = 0.02
+    
+    tick, status = get_tick()
+
+    
+    while status == 'ACTIVE':   
+        position = get_position() # this thing always returns absolute value
         
         # time_and_sales = get_time_sales('AC')
         
         # news = get_news()
+        tickers = ['CNR', 'RY', 'AC']
         
-        best_bid_price_AC, best_ask_price_AC = get_bid_ask('AC')
-        bid_ask_spread_AC = best_ask_price_AC - best_bid_price_AC
-        if bid_ask_spread_AC >= 0.1:
-            resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'AC', 'type': 'LIMIT', 'quantity': 500, 'price': best_bid_price_AC, 'action': 'BUY'})
-            resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'AC', 'type': 'LIMIT', 'quantity': 500, 'price': best_ask_price_AC, 'action': 'SELL'})
+        if position < MAX_EXPOSURE:
+            for i in range(len(tickers)):
+                best_bid_price, best_ask_price = get_bid_ask(tickers[i])
+                bid_ask_spread = best_ask_price - best_bid_price
+            
+                if bid_ask_spread >= 0.1:
+                    resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'AC', 'type': order_type, 'quantity': 100, 'price': best_bid_price, 'action': 'BUY'})
+                    resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'AC', 'type': order_type, 'quantity': 100, 'price': best_ask_price, 'action': 'SELL'})
+                     
         
-        best_bid_price_RY, best_ask_price_RY = get_bid_ask('RY')   
-        bid_ask_spread_RY = best_ask_price_RY - best_bid_price_RY         
-        if bid_ask_spread_RY >= 0.1:
-            resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'RY', 'type': 'LIMIT', 'quantity': 500, 'price': best_bid_price_RY, 'action': 'BUY'})
-            resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'RY', 'type': 'LIMIT', 'quantity': 500, 'price': best_ask_price_RY, 'action': 'SELL'})
-
-        best_bid_price_CNR, best_ask_price_CNR = get_bid_ask('CNR')     
-        bid_ask_spread_CNR = best_ask_price_CNR - best_bid_price_CNR
-        if bid_ask_spread_CNR >= 0.1:
-            resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'RY', 'type': 'LIMIT', 'quantity': 500, 'price': best_bid_price_CNR, 'action': 'BUY'})
-            resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'RY', 'type': 'LIMIT', 'quantity': 500, 'price': best_ask_price_CNR, 'action': 'SELL'})
-        
-        # If position is positive:
-            # sell market until net 0
-        # If position is Negative
-            # buy market until net 0
-
-        resp.json()
-        order_id = resp.json()['order_id']
+        # resp.json()
+        # order_id = resp.json()['order_id']
         
         # resp = s.delete('http://localhost:9999/v1/orders/' + str(order_id))
+        sleep(0.5)
         
         resp = s.post('http://localhost:9999/v1/commands/cancel', params = {'all': 1})
         
         # resp = s.post('http://localhost:9999/v1/commands/cancel', params = {'ticker': 'AC'})
     
         # resp = s.post('http://localhost:9999/v1/commands/cancel', params = {'query': 'Volume > 0'})
+        for i in range(len(tickers)):
+            ticker_position = get_position_ticker(i)
+            if ticker_position < 0:
+                resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'AC', 'type': 'MARKET', 'quantity': ticker_position, 'price': best_bid_price, 'action': 'BUY'})
+            if ticker_position > 0:
+                resp = s.post('http://localhost:9999/v1/orders', params = {'ticker': 'AC', 'type': 'MARKET', 'quantity': ticker_position, 'price': best_ask_price, 'action': 'SELL'})
         
         
         tick, status = get_tick()   
-        # x = 0
     
 
 if __name__ == '__main__':
